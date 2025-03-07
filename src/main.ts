dd739efbe063cc499c57d971c8bea9332739ec84
unknown <vaibhav.i@deepgrid.in>
2025-03-07 13:37:54 +0530

    CORS removed

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   }); // Enable detailed logs
-  app.enableCors({
-    origin: '*',
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
   await app.listen(5000);
   if (module.hot) {
     module.hot.accept();
