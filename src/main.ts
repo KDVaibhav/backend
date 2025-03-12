fbac80a24a0a9fcaf69d56a9b48c02c4f609d308
unknown <vaibhav.i@deepgrid.in>
2025-03-12 14:45:17 +0530

    client address changes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   }); // Enable detailed logs
+  app.use(cors({
+    origin: ["http://192.168.29.60:3000"],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  }));
+
   await app.listen(5000);
   if (module.hot) {
     module.hot.accept();
